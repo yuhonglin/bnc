22a2f0943b9ae86f390153ed18733c7537138e4a
Honglin Yu <yuhonglin1986@gmail.com>
2017-05-29 10:06:58 +1000

    add qgeom pgeom dgeom

--- src/dist/geom.hh
+++ src/dist/geom.hh
@@ -3,6 +3,12 @@
 
 #include <dist/pois.hh>
 
+extern "C" {
+    double bnc_dgeom(double x, double p, int give_log);
+    double bnc_pgeom(double x, double p, int lower_tail, int log_p);
+    double bnc_qgeom(double p, double prob, int lower_tail, int log_p);
+}
+
 namespace bnc {
 
     /* R functions */
@@ -16,6 +22,16 @@ namespace bnc {
     }
 
     R_RFUNC_INTERFACE_1ARG(rgeom);
+
+    /* D functions */
+    R_DFUNC_INTERFACE_3ARG(dgeom, bnc_dgeom);
+
+    /* P functions */
+    R_PFUNC_INTERFACE_4ARG(pgeom, bnc_pgeom);
+
+    /* Q functions */
+    R_QFUNC_INTERFACE_4ARG(qgeom, bnc_qgeom);
+    
     
 }  // namespace bnc
 
