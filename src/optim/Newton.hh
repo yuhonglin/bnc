812e961a008d9d3eb8ab61596511435ef7f82be0
Honglin Yu <yuhonglin1986@gmail.com>
2017-06-16 15:55:48 +1000

    add Newton Method

---
+++ src/optim/Newton.hh
@@ -0,0 +1,94 @@
+#ifndef NEWTON_H
+#define NEWTON_H
+
+#include <cmath>
+
+#include <util/logger.hh>
+#include <util/constant.hh>
+#include <optim/LineSearch/MoreThuente.hh>
+#include <optim/optim.hh>
+
+namespace bnc {
+    namespace optim {
+	template<typename LS>
+	class NewtonTemplate
+	{
+	public:
+	    template<typename F, typename G, typename H>
+	    static Result min(F f, G g, H h, const Vector& x0,
+			      const Vector& l, const Vector& u,
+			      const double& tol=DNTOL,
+			      const int& maxIter=1000) {
+		ASSERT_MSG( (l.array()<=x0.array()).all() &&
+			    (x0.array()<=u.array()).all(),
+			    "Infeasible inputs" );
+		Result res;
+		res.x = x0;
+		res.nIter = 0;
+		res.info = SUCCESS;
+
+		Vector dx(x0.size());
+		Vector direct(x0.size());
+		while (true) {
+		    res.nIter++;
+		    // compute direct and bound
+		    direct = -h(res.x).ldlt().solve(g(res.x));
+		    // check norm
+		    if (direct.norm() < 1e-15) {
+			res.f = f(res.x);
+			break;
+		    }
+		    direct.normalize();
+		    //
+		    double uu  = INF;
+		    double tmp = 0.;
+		    // Find the common limit (uu)
+		    // and handle the active contraints.
+		    // The active contraints only
+		    // exist and are only useful when uu=0.
+		    for (int i=0; i<x0.size(); i++) {
+			if (direct(i) > 0) {
+			    tmp = (u(i)-res.x(i))/direct(i);
+			} else {
+			    tmp = (l(i)-res.x(i))/direct(i);
+			}
+
+			if (eq(tmp,0,tol)) {
+			    // current contraint is active
+			    // set this direction to 0.
+			    direct(i) = 0.;
+			    continue;
+			} else {
+			    if (uu>tmp) uu = tmp;
+			}
+		    }
+
+		    uu = std::max(std::min(uu,1e15),0.);
+		    // search along direct
+		    double step = LS::search(f, g, res.x, direct,
+					     0., uu);
+		    dx = step*direct;
+		    res.x += step*direct;
+		    // check convergence
+		    if (dx.norm() <= tol) {
+			res.f = f(res.x);
+			break;
+		    }
+		    // check for maxIter
+		    if (res.nIter>=maxIter) {
+			res.info = MAXITERREACHED;
+			break;
+		    }
+		}
+
+		return res;
+	    }
+	    
+	}; // NewtonTemplate
+
+	using Newton = NewtonTemplate<optim::lsrch::MoreThuente>;
+	
+    }  // namespace optim
+}  // namespace bnc
+
+#endif /* NEWTON_H */
