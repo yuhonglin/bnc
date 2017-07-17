ced7fc4e3a31b6460d8f66e107612b76b903a09b
Honglin Yu <yuhonglin1986@gmail.com>
2017-07-17 12:33:41 +1000

    add inverse gamma
    only have the simplest d-function for now

---
+++ src/dist/invgamma.hh
@@ -0,0 +1,37 @@
+#ifndef INVGAMMA_H
+#define INVGAMMA_H
+
+#include <cmath>
+
+#include <util/logger.hh>
+#include <util/constant.hh>
+#include <dist/brutil.hh>
+
+extern "C" {
+    double lgamma(double x);
+}
+
+namespace bnc {
+    /* D function */
+    double dinvgamma(const double& x, const double &a,
+		     const double&scale, const int& give_log) {
+	if (shape <=0 or scale <= 0) {
+	    LOG_WARNING("Invalid inputs, return NaN");
+	    return NaN;
+	}
+
+	const double alpha = shape;
+	const double beta = scale;
+	
+	double log_density = alpha * std::log(beta)
+	    - lgamma(alpha) - (alpha+1)*std::log(x) - (beta/x);
+
+	if (give_log==0) {
+	    return std::exp(log_density);
+	} else {
+	    return log_density;
+	}
+    }
+}  // namespace bnc
+
+#endif /* INVGAMMA_H */
